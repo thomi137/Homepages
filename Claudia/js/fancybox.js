2083611565c3fc249653dc5e58f9160ac757cb98
thomi137 <thomas.prosser@coloruapp.com>
2013-10-30 19:26:11 +0100

    Put under a webserver

---
+++ Claudia/js/fancybox.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+			$("a[rel=example_group]").fancybox({
+				'transitionIn'		: 'none',
+				'transitionOut'		: 'none',
+				'titlePosition' 	: 'outside',
+				'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
+					return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+				}
+			});
+
+		});
