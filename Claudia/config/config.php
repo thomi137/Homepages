f0fdd37f766239c3311396cab6d362384cc7e44f
thomi137 <thomas@prosser.ch>
2013-11-21 20:26:30 +0100

    Added new configuration

--- Claudia/config/config.php
+++ Claudia/config/config.php
@@ -1,11 +1,15 @@
 <?php
+/* Globals */
+define('ROOT', getenv('DOCUMENT_ROOT').'/Claudia/');
+
 /* Setup of all pages navigation */
 $pages = array();
-$pages['gallery'] = 'content/gallery.php';
-$pages['contact'] = 'content/contact.php';
+$pages['gallery'] = ROOT.'content/gallery.php';
+$pages['contact'] = ROOT.'content/contact.php';
+$pages['upload'] = ROOT.'content/upload.php';
 
 /* Setup of media content path */
 $paths = array();
-$paths['pictures'] = 'gallery/pictures/';
-$paths['thumbnails'] = 'gallery/thumbnails/';
+$paths['pictures'] = ROOT.'gallery/pictures/';
+$paths['thumbnails'] = ROOT.'gallery/thumbnails/';
 ?>
