632828011259bfbbf2fbd2ada5e08a07e6552721
thomi137 <thomas@prosser.ch>
2013-11-22 19:19:06 +0100

    New image resize script to create resized image and thumbnail for the Homepage, Moved all template and presentation related stuff to own folder so as to have an admin template and a frontpage template

---
+++ Claudia/template/front/head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Name of Page </title>
+    <meta charset="utf-8"/>
+	<link rel="stylesheet" type="text/css" href="./template/front/css/layout.css">
+	<script src="./js/jquery-1.4.3.min.js"></script>
+	<script type="text/javascript" src="./template/front/css/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
+	<script type="text/javascript" src="./template/front/css/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+	<link rel="stylesheet" type="text/css" href="./template/front/css/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+
+    </head>
+	<body>
+		<!--[if gte IE 9]>
+			<style type="text/css">
+			#header {
+				filter: none;
+			}
+			</style>
+			<![endif]-->
